15bdb2b1ad0a58f39145ba30f3e67c7f16caa53e
RachaelCheung <rcheung00@stuy.edu>
2019-02-28 14:03:59 -0500

    Rachael edited

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,7 +1,7 @@
 public class UserOfSpeakers{
     public static void main(String[] commandLine){
       Animal myPet = new Retriever();
-      mypet.speak();
+      System.out.println(myPet.speak());
 
 
 
